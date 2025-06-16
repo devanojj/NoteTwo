2d1bfe8c78aa06a5ba1ab88d2cf0e5d333f55ac4
devanojj <devanojose4@gmail.com>
2025-06-16 21:25:06 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const savedNote = localStorage.getItem('note');
+    if (savedNote) {
+      setText(savedNote);
+    }
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('note', text);
+  }, [text]);
+
+
+    return (
+    <textarea
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      
+      style={{
+        width: '100vw',         // Full width
+        height: '100vh',        // Full height
+        border: 'none',
+        outline: 'none',
+        padding: '1rem',
+        fontSize: '1rem',
+        fontFamily: 'monospace', // Like Notepad
+        lineHeight: '1.5',
+        resize: 'none',
+        boxSizing: 'border-box', // Ensures padding doesn't shrink the box
+      }}
+      />
+  );
+}
+
+export default App;
